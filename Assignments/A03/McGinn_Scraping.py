f7f196f5f2f4f662668483a2073a8552f32ef352
dnmcginn57 <nicholasmcginn57@yahoo.com>
2019-02-05 18:07:42 -0600

    scraping done, git ignore added

--- Assignments/A03/McGinn_Scraping.py
+++ Assignments/A03/McGinn_Scraping.py
@@ -3,20 +3,18 @@ from pprint import pprint
 import urllib
 import json
 import sys
+import requests
 from time import sleep
 from random import shuffle
-
 scraper = BeautifulScraper()
 
 
 
-years = [x+1 for x in range(1969,2019)] 
+years = [x+1 for x in range(2008,2019)] 
 stype = ""
 weeks = [x+1 for x in range(18)]
 
 delays = [.01,.02,.03,.04,.05]
-#url = "http://www.nfl.com/schedules/%d/%s%s" % (sYear,stype,str(week))
-#url2 = "http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json"
 
 gameIDs = []
 #iterate for every year
@@ -29,43 +27,36 @@ for year in years:
         print(week)
         #all divs with the data-gameid in them
         idDivs = page.find_all("div", {'class':'schedules-list-content'})
-        #a ham-fisted and stupid way to fix this problem
-        #idDivsAlt = page.find_all("div", {'class':'schedules-list-content post expandable primetime type-reg pro-legacy'})
-        #idDivsAlt2 = page.find_all("div", {'class':'schedules-list-content post expandable type-reg pro-legacy'})
         #add all the found game id's to gameIDs[]
         for div in idDivs:
             gameIDs.append(div['data-gameid'])
             print(".")
-        #for div in idDivsAlt:
-        #    gameIDs.append(div['data-gameid'])
-        #    print(".")
-        #for div in idDivsAlt2:
-        #    gameIDs.append(div['data-gameid'])
-        #    print(".")
+
     
     #now just do post season id's by themselves for each year
     url = "http://www.nfl.com/schedules/%d/POST" % (year)
     page = scraper.go(url)
     idDivs = page.find_all("div", {'class':'schedules-list-content'})
-    #idDivsAlt = page.find_all("div", {'class':'schedules-list-content post expandable primetime type-reg pro-legacy'})
-    #idDivsAlt2 = page.find_all("div", {'class':'schedules-list-content post expandable type-reg pro-legacy'})
+
     #add all the found game id's to gameIDs[]
     for div in idDivs:
         gameIDs.append(div['data-gameid'])
-   
-    #for div in idDivsAlt:
-    #    gameIDs.append(div['data-gameid'])
-
-    #for div in idDivsAlt2:
-    #    gameIDs.append(div['data-gameid'])
         
         
-
+#gameIDs is 2680 which I think is a good number
 print(len(gameIDs))
 
-#after much debugging, it is determined gameIDs has the right number of game id's in it
+#now to access all of the live data sites and save them for later use
 
-#generate urls for live update from id's in gameIDs and consolidate them into a single json file
+url = "http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json"
 
 
+for gid in gameIDs:
+    gameData = requests.get(url % (str(gid), str(gid)))
+    gDataJSON = gameData.json()
+    filePath = "./game_data/%s.json" % (str(gid))
+    shuffle(delays)
+    sleep(delays[1])
+    with open(filePath, 'w') as fp:
+        json.dump(gDataJSON,fp)
 
